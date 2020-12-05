812926e7910e956a7088c2b24c5a72aa1a90959b
luizgodoy26 <luizgodoy26@gmail.com>
2020-12-05 14:32:45 -0300

    Ajustando heroku

--- oxm_crm/settings.py
+++ oxm_crm/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['oxm-crm.herokuapp.com/']
+ALLOWED_HOSTS = ['oxm-crm.herokuapp.com']
 
 
 # Application definition
